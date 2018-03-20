841bd1013aeebc78ab1143c59efbb9d3de91cdac
kylemccann <kylemcc08@gmail.com>
2018-03-20 22:49:14 +0000

    added adapter-latest.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatExpansionModule} from '@angular/material/expansion';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReadMoreDirective } from '../assets/ngx-read-more';
 import {
+  MatButtonModule,
   MatMenuModule,
   MatToolbarModule,
   MatIconModule,
@@ -15,10 +12,16 @@ import {
 } from '@angular/material';
 
 
+
+
+
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent,
-    ReadMoreDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -27,8 +30,6 @@ import {
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
-    MatExpansionModule,
-    BrowserAnimationsModule,
 
     // Recorder,
     // RecordRTC
